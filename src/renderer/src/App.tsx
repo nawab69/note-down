ffc9e7308ca60742d1afebcfbeed74f3c8b7d0be
Nawab Khairuzzaman <nawabkhairuzzaman@gmail.com>
2024-07-08 12:45:10 +0600

    styling setup done (tailwindcss)

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,9 @@
 function App(): JSX.Element {
-  return <></>
+  return (
+    <div className="flex h-full items-center justify-center">
+      <span className="text-blue-500 text-4xl">Hello from electron</span>
+    </div>
+  )
 }
 
 export default App
